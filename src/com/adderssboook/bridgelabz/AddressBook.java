9671e7e242bae6c0650d7cb190a3a6b5d8b2cf5e
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-28 12:33:13 +0530

    Edit contact details

--- src/com/adderssboook/bridgelabz/AddressBook.java
+++ src/com/adderssboook/bridgelabz/AddressBook.java
@@ -6,10 +6,8 @@ public class AddressBook {
 	// instance variable;
 	Contact contact;
 
-	public void addContact() {
-
+	public void addNewContact() {
 		Scanner scanner = new Scanner(System.in);
-
 		System.out.println("enter the first name");
 		String firstName = scanner.nextLine();
 		System.out.println("enter the last name");
@@ -28,19 +26,52 @@ public class AddressBook {
 		String email = scanner.next();
 
 		contact = new Contact(firstName, lastName, address, state, city, zipCode, number, email);
-		System.out.println("contact details added successfully");
+		System.out.println("contact successfully added");
+	}
+	
+	public void editContactDetails(String firstName) {
+		if (contact == null)
+			System.out.println("Address book is empty");
+		else {
+			if (contact.getFirstName().equalsIgnoreCase(firstName)) {
+				System.out.println("edit the contact details");
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("enter the first name");
+				contact.setFirstName(scanner.next());
+				System.out.println("enter the last name");
+				contact.setLastName(scanner.next());
+				System.out.println("enter address");
+				contact.setAddress(scanner.next());
+				System.out.println("enter the state");
+				contact.setState(scanner.next());
+				System.out.println("enter the city");
+				contact.setCity(scanner.next());
+				System.out.println("enter the zipcode");
+				contact.setZipCode(scanner.nextInt());
+				System.out.println("enter mobile number");
+				contact.setNumber(scanner.nextLong());
+				System.out.println("enter the email-id");
+				contact.setEmail(scanner.next());
 
+				System.out.println("Contact successfully updated");
+			} else
+				System.out.println("contact not found");
+		}
 	}
 
+	// to print the contact details
 	@Override
 	public String toString() {
-		return "AddressBook [contact=" + contact + "]";
+		return contact.toString();
 	}
+
+	
+
 
 	public static void main(String args[]) {
 		System.out.println("Welcome address book program !!");
 		AddressBook addressBook = new AddressBook();
-		addressBook.addContact();
+		addressBook.addNewContact();
 		System.out.println(addressBook.toString());
 	}
 
--- src/com/adderssboook/bridgelabz/Contact.java
+++ src/com/adderssboook/bridgelabz/Contact.java
@@ -1,5 +1,7 @@
 package com.adderssboook.bridgelabz;
 
+import java.util.Scanner;
+
 public class Contact {
 	// instance variable declare
 
@@ -25,10 +27,76 @@ public class Contact {
 		this.email = email;
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public long getNumber() {
+		return number;
+	}
+
+	public void setNumber(long number) {
+		this.number = number;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	@Override
 	public String toString() {
 		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
 				+ state + ", city=" + city + ", zipCode=" + zipCode + ", number=" + number + ", email=" + email + "]";
 	}
-
+   
 }
+	
+
