532af64068d71d00b6137208443210651dcf3ba0
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-28 08:39:29 +0530

    Contacts with multiple information

--- src/com/adderssboook/bridgelabz/AddressBook.java
+++ src/com/adderssboook/bridgelabz/AddressBook.java
@@ -4,6 +4,9 @@ public class AddressBook {
 	
 		public static void main(String args[]) {
 			System.out.println("Welcome address book program !!");
+			Contact contact = new Contact("Allu", "Arjun", "Banglore", "Karnataka", "Karnataka",
+					560068, 9152678740L, "allu.arjun@gmail.com");
+			System.out.println(contact.toString());
 	}
 
 }
---
+++ src/com/adderssboook/bridgelabz/Contact.java
@@ -0,0 +1,32 @@
+package com.adderssboook.bridgelabz;
+
+public class Contact {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String state;
+	private String city;
+	private int zipCode;
+	private long number;
+	private String email;
+
+	public Contact(String firstName, String lastName, String address, String state, String city, int zipCode,
+			long number, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.state = state;
+		this.city = city;
+		this.zipCode = zipCode;
+		this.number = number;
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
+				+ state + ", city=" + city + ", zipCode=" + zipCode + ", number=" + number + ", email=" + email + "]";
+	}
+
+}
